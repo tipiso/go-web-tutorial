f8f0541ad7d68c7f7eafae2778e69fe596faff97
artur.sokalski <artur.sokalski@zaven.pl>
2024-01-09 12:57:48 +0100

    refactor: Separated books endpoints to a dedicated func

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module web-tut/main
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -3,16 +3,53 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create Book")
+}
+
+func AllBooks(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create Book")
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create Book")
+}
+
+func RegisterBooksRouter(r *mux.Router) {
+	// Specific path restriction
+	bookrouter := r.PathPrefix("/books").Subrouter()
+	bookrouter.HandleFunc("/", AllBooks).Methods("GET")
+	bookrouter.HandleFunc("/{title}", GetBook).Methods("GET")
+
+	// Example route handlers
+	bookrouter.HandleFunc("/{title}", CreateBook).Methods("POST")
+	// r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+	// r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+	// r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+
+	bookrouter.HandleFunc("/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+}
+
 func main() {
-	
+	r := mux.NewRouter()
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	RegisterBooksRouter(r)
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hoy, Info from server!!!")
 	})
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", r)
 }
